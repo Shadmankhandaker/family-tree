a398dcd32f30c466155afed964b61d0548f0b8b9
Syed Noor E Newaz <63665452+JimNewaz@users.noreply.github.com>
2021-09-15 12:31:43 +0600

    Update links.php

--- includes/links.php
+++ includes/links.php
@@ -13,6 +13,7 @@
 <!-- Main css File -->
 <link href="style.css" type="text/css" rel="stylesheet" media="screen,projection" />
 <link href="css/card.css" rel="stylesheet">
+<link href="css/scroll.css" rel="stylesheet">
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
